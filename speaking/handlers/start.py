ac60cc482e33edf9d8487fb16cd9e95cd29d8f80
Elizaveta <lizasalnikova2007@icloud.com>
2026-04-10 18:51:45 +0500

    add buttons

--- speaking/handlers/start.py
+++ speaking/handlers/start.py
@@ -1,4 +1,25 @@
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 async def start(message: Message):
-    await message.answer("Привет! 👋 Бот работает на aiogram 🚀")
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="🎮 Игры", callback_data="games"),
+        ],
+        [
+            InlineKeyboardButton(text="📝 ОГЭ / ЕГЭ", callback_data="exam"),
+        ],
+        [
+            InlineKeyboardButton(text="🎤 Speaking", callback_data="speaking"),
+        ]
+    ])
+
+    text = (
+        "Hello!👋 Я твой персональный учитель английского 🇬🇧\n\n"
+        "Я помогу тебе:\n"
+        "— прокачать разговорный английский\n"
+        "— подготовиться к экзаменам\n"
+        "— играть и учить слова\n\n"
+        "👇 Выбери режим:"
+    )
+
+    await message.answer(text, reply_markup=keyboard)
