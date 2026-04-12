79582413445c922362c230ff81822ea5c84158db
Elizaveta <lizasalnikova2007@icloud.com>
2026-04-13 01:29:20 +0500

    fix imports to speaking.* (final)

--- speaking/main.py
+++ speaking/main.py
@@ -2,18 +2,13 @@ import asyncio
 import threading
 import http.server
 import socketserver
-import sys
-import os
-
-# 🔥 фикс путей (ЭТО КЛЮЧЕВОЕ)
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from aiogram import Bot, Dispatcher
 
-from handlers.start import router as start_router
-from handlers.voice import router as voice_router
+from speaking.handlers.start import router as start_router
+from speaking.handlers.voice import router as voice_router
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8652892060:AAGnlfueIW4WVenereDZjRjV3E0dOuHu8vg"
 
 
 def start_dummy_server():
