d2ffde93ae15d55e46372d10290331fdfa11a1e2
Elizaveta <lizasalnikova2007@icloud.com>
2026-04-13 12:16:15 +0500

    add main file

---
+++ speaking/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import threading
+import http.server
+import socketserver
+
+from aiogram import Bot, Dispatcher
+
+from speaking.handlers.start import router as start_router
+from speaking.handlers.voice import router as voice_router
+
+BOT_TOKEN = "8652892060:AAGnlfueIW4WVenereDZjRjV3E0dOuHu8vg"
+
+
+def start_dummy_server():
+    PORT = 10000
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        httpd.serve_forever()
+
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(start_router)
+    dp.include_router(voice_router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    threading.Thread(target=start_dummy_server).start()
+    asyncio.run(main())
