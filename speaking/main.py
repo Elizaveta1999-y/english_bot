716d62cbcadd6c02a94eeab54ebd7690882044b2
Elizaveta <lizasalnikova2007@icloud.com>
2026-04-14 02:10:51 +0500

    fix: remove telegram conflict

--- speaking/main.py
+++ speaking/main.py
@@ -1,7 +1,4 @@
 import asyncio
-import threading
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
 from aiogram import Bot, Dispatcher
 
 from speaking.handlers.start import router as start_router
@@ -10,33 +7,21 @@ from speaking.handlers.voice import router as voice_router
 BOT_TOKEN = "8652892060:AAGnlfueIW4WVenereDZjRjV3E0dOuHu8vg"
 
 
-# 🔥 ФЕЙКОВЫЙ СЕРВЕР ДЛЯ RENDER
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"OK")
-
-
-def run_fake_server():
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(("0.0.0.0", port), Handler)
-    server.serve_forever()
-
-
 async def main():
     bot = Bot(token=BOT_TOKEN)
+
+    # 💣 УБИВАЕМ ВСЕ СТАРЫЕ СЕССИИ
+    await bot.delete_webhook(drop_pending_updates=True)
+
     dp = Dispatcher()
 
     dp.include_router(start_router)
     dp.include_router(voice_router)
 
+    print("BOT STARTED")
+
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    # 👇 запускаем фейковый сервер в отдельном потоке
-    threading.Thread(target=run_fake_server, daemon=True).start()
-
     asyncio.run(main())
