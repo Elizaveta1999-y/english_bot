a5d88b62467f347845ec03eba902aea049e9e25e
Elizaveta <lizasalnikova2007@icloud.com>
2026-04-15 15:26:10 +0500

    fix: add fake web server for render

--- speaking/main.py
+++ speaking/main.py
@@ -1,24 +1,44 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
+from aiohttp import web
 
 from speaking.handlers.start import router as start_router
 from speaking.handlers.voice import router as voice_router
 
-BOT_TOKEN = "8652892060:AAGnlfueIW4WVenereDZjRjV3E0dOuHu8vg"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+dp.include_router(start_router)
+dp.include_router(voice_router)
 
 
+# ====== ФЕЙКОВЫЙ СЕРВЕР ДЛЯ RENDER ======
+async def handle(request):
+    return web.Response(text="Bot is running")
+
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", handle)
+
+    port = int(os.environ.get("PORT", 10000))
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+
+
+# ====== ЗАПУСК ======
 async def main():
-    bot = Bot(token=BOT_TOKEN)
+    await start_web_server()
 
-    # 💣 УБИВАЕМ ВСЕ СТАРЫЕ СЕССИИ
-    await bot.delete_webhook(drop_pending_updates=True)
-
-    dp = Dispatcher()
-
-    dp.include_router(start_router)
-    dp.include_router(voice_router)
-
-    print("BOT STARTED")
+    await bot.set_my_commands([
+        BotCommand(command="start", description="Start bot"),
+    ])
 
     await dp.start_polling(bot)
 
