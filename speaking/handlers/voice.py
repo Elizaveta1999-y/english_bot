d55caf59ebd876712d9d2dcf6bcd3b8bd7ee769d
Elizaveta <lizasalnikova2007@icloud.com>
2026-05-20 03:17:06 +0500

    fix: add universal text handler

--- speaking/handlers/voice.py
+++ speaking/handlers/voice.py
@@ -3,5 +3,5 @@ from aiogram import Router, types
 router = Router()
 
 @router.message()
-async def echo(message: types.Message):
-    await message.answer(f"Echo: {message.text}")
+async def echo_all(message: types.Message):
+    await message.answer(f"✅ Получено: {message.text}")
