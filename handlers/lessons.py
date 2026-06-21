86342b67f87de5d5b3392d79f268bd0b5e62b2d4
Elizaveta <lizasalnikova2007@icloud.com>
2026-06-21 19:05:52 +0500

    Исправлена синтаксическая ошибка: добавлена запятая в словаре

--- handlers/lessons.py
+++ handlers/lessons.py
@@ -883,7 +883,7 @@ async def lesson_practice_start(callback: CallbackQuery):
             "session_correct": 0,
             "skip_count": 0,
             "attempts": {},
-            "variant_index": variant_index
+            "variant_index": variant_index,
             "total_subtasks": sum(len(t.get("subtasks", [])) for t in tasks)
         }
         user_state["practice_variant"][key] = variant_index
