0d9cab6ebd0fda0b93ab9489cae1de9eb6ac7fe9
Elizaveta <lizasalnikova2007@icloud.com>
2026-06-24 02:54:08 +0500

    Исправлены отступы в level_a2.py

--- data/level_a2.py
+++ data/level_a2.py
@@ -5390,7 +5390,7 @@ LEVEL_A2_CONTENT = LEVEL_A2_CONTENT = {
   ]
 }
   },
-  "modal_should_ought": {
+ "modal_should_ought": {
     "title": "Модальные глаголы: should, ought to",
     "pages": [
       {
@@ -20517,3 +20517,4 @@ LEVEL_A2_CONTENT = LEVEL_A2_CONTENT = {
     ]
   ]
 }
+
