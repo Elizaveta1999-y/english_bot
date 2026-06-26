d7c772bf83ecbab9f89dd1139b08e3a8be70ce0b
Elizaveta <lizasalnikova2007@icloud.com>
2026-06-26 16:38:34 +0500

    Обновлён lessons.py (практика для новых уроков)

--- handlers/lessons.py
+++ handlers/lessons.py
@@ -7,7 +7,7 @@ from data.level_a1 import LEVEL_A1_CONTENT
 from data.level_a2 import LEVEL_A2_CONTENT
 from data.level_b1 import LEVEL_B1_CONTENT
 from data.level_b2 import LEVEL_B2_CONTENT
-# from data.level_c1 import LEVEL_C1_CONTENT
+from data.level_c1 import LEVEL_C1_CONTENT
 # from data.level_c2 import LEVEL_C2_CONTENT
 # from data.thematic_new import THEMATIC_NEW_CONTENT
 from services.deepseek import chat
@@ -100,6 +100,7 @@ LESSON_CONTENT.update(LEVEL_A1_CONTENT)
 LESSON_CONTENT.update(LEVEL_A2_CONTENT)
 LESSON_CONTENT.update(LEVEL_B1_CONTENT)
 LESSON_CONTENT.update(LEVEL_B2_CONTENT)
+LESSON_CONTENT.update(LEVEL_C1_CONTENT)
 THEMATIC_TOPICS = [
     "Inversion after negative adverbs",
     "Inversion for emphasis",
